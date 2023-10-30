59c555f0b1ec1f2df4313d0b5e36fb9750545963
Akane Beneckendorff <akane@akanework.org>
2023-10-30 12:06:04 +0800

    Checker: Bump version to `1.0.2`
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.akanework.checker"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
